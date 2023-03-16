4e7d6ff5a78e37c52c6e8c7bf4e1feffbb0f671b
Nizar-Amine1 <nizaramine11@hotmail.com>
2023-03-17 00:22:43 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
 #include <unistd.h>
 /**
- *  * main - Entry point
- *   * Return:1
-     */
+ * * main - Entry point
+ * *
+ * * Description: 'the program's description'
+ * *
+ * * Return: Always 0 (Success)
+  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
+
+
 }
