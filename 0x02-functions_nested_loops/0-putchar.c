7af6ac22c2d0cce7a15d45d96648f25f971c6ecb
Nizar-Amine1 <nizaramine11@hotmail.com>
2023-03-21 22:58:04 +0100

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,14 @@
 #include "main.h"
+
+
 /**
 * main - Entry point
 *
 * Return: Always 0 (Success)
 */
+
 int main(void)
 {
-
 	_putchar ('_');
 	_putchar ('p');
 	_putchar ('u');
@@ -18,9 +20,4 @@ int main(void)
 	_putchar ('\n');
 
 	return (0);
-
-
-
-
-
 }
